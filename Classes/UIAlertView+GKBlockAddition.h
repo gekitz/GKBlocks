79ab70317fb3d624968cd76b75dd03e52a713d52
Georg Kitz <georgkitz@gmail.com>
2014-07-03 11:02:24 +0100

    removed duplicated files

--- Classes/UIAlertView+GKBlockAddition.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Georg Kitz on 19/05/14.
-// Copyright (c) 2014 Aurora Apps. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-@interface UIAlertView (GKBlockAddition)
-@end
